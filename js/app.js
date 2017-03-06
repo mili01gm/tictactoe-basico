45dc77b1d21a07abb6969b20e91993e379108ea9
Luz Gutierrez <mili01gm@gmail.com>
2017-03-05 22:56:10 -0500

    Ya funciona esta cosagit push origin funcionesgit push origin funciones

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 //Todas las variables.TODAS.
+//Para las partidas
 var j1 = document.getElementById('j1');
 var j2 = document.getElementById('j2');
 var j3 = document.getElementById('j3');
@@ -8,44 +9,56 @@ var j6 = document.getElementById('j6');
 var j7 = document.getElementById('j7');
 var j8 = document.getElementById('j8');
 var j9 = document.getElementById('j9');
+//Para los turnos
 var turno = false;
 var buton = document.getElementsByClassName('buttons');
-//var reset = document.getElementById('reset');
-//var playerX;
-//var playerO;
-//var xWins = 0;
-//var oWins = 0;
+//var botones = {j1,j2,j3,j4,j5,j6,j7,j8,j9};
+var reset = document.getElementById('reset');
+//Para el score
+var playerX = false;
+var playerO = false;
+var xWins = 0;
+var oWins = 0;
 
 
 // turnos con .setAttributte() info en https://www.w3schools.com/jsref/met_element_setattribute.asp
-function playingTurn() {
-      if(turno === false){
-      this.setAttribute('value', "x");
-      getWinner();
-      turno = true;
-      return true;}
-    if(turno === true){
-      this.setAttribute('value', "o");
-      getWinner();
-      turno = false;}
+function playingTurn(){
+  //alert("hola");
+  if(turno == false){
+    //var i;
+    //for(i in botones){i.setAttribute("value","x");}
+    this.setAttribute("value","x");
+    getWinner();
+    turno = true;
+    return true;}
+  else if(turno == true){
+    this.setAttribute("value","o");
+    getWinner();
+    turno = false;
+  }
 }
 
+//Se llama a cada turno
+for(var i = 0; i < buton.length; i+=1){
+  buton[i].addEventListener('click',playingTurn);
+}
+
+//TODAS las posibles partidas. En principio.
 function getWinner(){
-  "use strict";
-  if((j1.value == "x" && j2.value == "x" && j3.value == "x")||
-     (j4.value == "x" && j5.value == "x" && j6.value == "x")||
-     (j7.value == "x" && j8.value == "x" && j9.value == "x")||
-     (j1.value == "x" && j4.value == "x" && j7.value == "x")||
-     (j2.value == "x" && j5.value == "x" && j8.value == "x")||
-     (j3.value == "x" && j6.value == "x" && j9.value == "x")||
-     (j1.value == "x" && j5.value == "x" && j9.value == "x")||
-     (j3.value == "x" && j5.value == "x" && j7.value == "x")){
+  if(((j1.value == "x") && (j2.value == "x") && (j3.value == "x"))||
+     ((j4.value == "x") && (j5.value == "x") && (j6.value == "x"))||
+     ((j7.value == "x") && (j8.value == "x") && (j9.value == "x"))||
+     ((j1.value == "x") && (j4.value == "x") && (j7.value == "x"))||
+     ((j2.value == "x") && (j5.value == "x") && (j8.value == "x"))||
+     ((j3.value == "x") && (j6.value == "x") && (j9.value == "x"))||
+     ((j1.value == "x") && (j5.value == "x") && (j9.value == "x"))||
+     ((j3.value == "x") && (j5.value == "x") && (j7.value == "x"))){
        alert("El jugador X gana!");
-       //playerX = true;
-       //gameScore();
+       playerX = true;
+       gameScore();
        return true;
      }
-    /*else*/ if((j1.value == "o" && j2.value == "o" && j3.value == "o")||
+    if((j1.value == "o" && j2.value == "o" && j3.value == "o")||
        (j4.value == "o" && j5.value == "o" && j6.value == "o")||
        (j7.value == "o" && j8.value == "o" && j9.value == "o")||
        (j1.value == "o" && j4.value == "o" && j7.value == "o")||
@@ -54,11 +67,11 @@ function getWinner(){
        (j1.value == "o" && j5.value == "o" && j9.value == "o")||
        (j3.value == "o" && j5.value == "o" && j7.value == "o")){
          alert("El jugador O gana!");
-         //playerO = true;
-         //gameScore();
+         playerO = true;
+         gameScore();
          return true;
        }
-    /*else*/ if((j1.value=="x" || j1.value=="o")&&
+    if((j1.value=="x" || j1.value=="o")&&
             (j2.value=="x" || j2.value=="o")&&
 			      (j3.value=="x" || j3.value=="o")&&
             (j4.value=="x" || j4.value=="o")&&
@@ -72,12 +85,9 @@ function getWinner(){
             }
 }
 
-/*function gameScore(){
-		if(playerX === true){xWins++; xScore.innerHTML = xWins;}
+// Se guarda la partida ganada en el span.
+function gameScore(){
+		if(playerX === true){
+      xWins++; xScore.innerHTML = xWins;}
     else if(playerO === true){oWins++; oScore.innerHTML = oWins;}
-}*/
-
-
-for(var i = 1; i < buton.length; i++){
-  buton(i).addEventListener('click', playingTurn);
 }
