a9d590ed2c90b22d1a8a1b9c82cfa07be5970afd
Luz Gutierrez <mili01gm@gmail.com>
2017-03-05 21:47:53 -0500

    subiendo los intentos a la rama

--- js/app.js
+++ js/app.js
@@ -1,4 +1,83 @@
-var Jugador = {
-  [name:'Jugador1',symbol:'X',turnos: {1,3,5,7,9}],
-  [name: 'Jugador2',symbol: 'Y',turnos: {2,4,6,8}]
+//Todas las variables.TODAS.
+var j1 = document.getElementById('j1');
+var j2 = document.getElementById('j2');
+var j3 = document.getElementById('j3');
+var j4 = document.getElementById('j4');
+var j5 = document.getElementById('j5');
+var j6 = document.getElementById('j6');
+var j7 = document.getElementById('j7');
+var j8 = document.getElementById('j8');
+var j9 = document.getElementById('j9');
+var turno = false;
+var buton = document.getElementsByClassName('buttons');
+//var reset = document.getElementById('reset');
+//var playerX;
+//var playerO;
+//var xWins = 0;
+//var oWins = 0;
+
+
+// turnos con .setAttributte() info en https://www.w3schools.com/jsref/met_element_setattribute.asp
+function playingTurn() {
+      if(turno === false){
+      this.setAttribute('value', "x");
+      getWinner();
+      turno = true;
+      return true;}
+    if(turno === true){
+      this.setAttribute('value', "o");
+      getWinner();
+      turno = false;}
+}
+
+function getWinner(){
+  "use strict";
+  if((j1.value == "x" && j2.value == "x" && j3.value == "x")||
+     (j4.value == "x" && j5.value == "x" && j6.value == "x")||
+     (j7.value == "x" && j8.value == "x" && j9.value == "x")||
+     (j1.value == "x" && j4.value == "x" && j7.value == "x")||
+     (j2.value == "x" && j5.value == "x" && j8.value == "x")||
+     (j3.value == "x" && j6.value == "x" && j9.value == "x")||
+     (j1.value == "x" && j5.value == "x" && j9.value == "x")||
+     (j3.value == "x" && j5.value == "x" && j7.value == "x")){
+       alert("El jugador X gana!");
+       //playerX = true;
+       //gameScore();
+       return true;
+     }
+    /*else*/ if((j1.value == "o" && j2.value == "o" && j3.value == "o")||
+       (j4.value == "o" && j5.value == "o" && j6.value == "o")||
+       (j7.value == "o" && j8.value == "o" && j9.value == "o")||
+       (j1.value == "o" && j4.value == "o" && j7.value == "o")||
+       (j2.value == "o" && j5.value == "o" && j8.value == "o")||
+       (j3.value == "o" && j6.value == "o" && j9.value == "o")||
+       (j1.value == "o" && j5.value == "o" && j9.value == "o")||
+       (j3.value == "o" && j5.value == "o" && j7.value == "o")){
+         alert("El jugador O gana!");
+         //playerO = true;
+         //gameScore();
+         return true;
+       }
+    /*else*/ if((j1.value=="x" || j1.value=="o")&&
+            (j2.value=="x" || j2.value=="o")&&
+			      (j3.value=="x" || j3.value=="o")&&
+            (j4.value=="x" || j4.value=="o")&&
+            (j5.value=="x" || j5.value=="o")&&
+            (j6.value=="x" || j6.value=="o")&&
+            (j7.value=="x" || j7.value=="o")&&
+            (j8.value=="x" || j8.value=="o")&&
+            (j9.value=="x" || j9.value=="o")){
+                alert("Es un empate :(");
+                return true;
+            }
+}
+
+/*function gameScore(){
+		if(playerX === true){xWins++; xScore.innerHTML = xWins;}
+    else if(playerO === true){oWins++; oScore.innerHTML = oWins;}
+}*/
+
+
+for(var i = 1; i < buton.length; i++){
+  buton(i).addEventListener('click', playingTurn);
 }
